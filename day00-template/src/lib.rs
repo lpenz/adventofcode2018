32a055f6313802052599e0456190509161d4e27c
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-01-11 18:46:48 -0300

    Add a dummy "Day 00" as a template

---
+++ day00-template/src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright (C) 2021 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use anyhow::Result;
+use std::io::BufRead;
+
+pub const EXAMPLE: &str = "
+";
+
+pub fn parse(bufin: impl BufRead) -> Result<Vec<String>> {
+    bufin
+        .lines()
+        .map(|line_opt| {
+            let line = line_opt?;
+            Ok(line)
+        })
+        .collect()
+}
+
+#[test]
+fn test() -> Result<()> {
+    assert_eq!(parse(EXAMPLE.as_bytes())?, &[""]);
+    Ok(())
+}
